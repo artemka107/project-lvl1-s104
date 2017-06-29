3528f24ba7986d616913bbb35a7a139c436db459
artem <artem.ivanec96@gmail.com>
2017-06-29 18:47:50 +0300

    added tests and linter

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ const welcome = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   return console.log(`Hello, ${name}`);
-}
+};
 
 export default welcome;
