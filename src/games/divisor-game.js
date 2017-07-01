c39bc4872f0235c0701c07ae4c0d529bac8f9480
Artem <artem.ivanec96@gmail.com>
2017-07-01 16:11:15 +0300

    added GCD task

---
+++ src/games/divisor-game.js
@@ -0,0 +1,27 @@
+import { cons, car, cdr } from 'hexlet-pairs';
+import { welcome, gameProcess } from '../user-logic';
+
+const rule = 'Find the greatest common divisor of given numbers';
+
+const generateTask = () => {
+  const numbers = cons(Math.ceil(Math.random() * 100), Math.ceil(Math.random() * 100));
+  let a = car(numbers);
+  let b = cdr(numbers);
+
+  while (a !== 0 && b !== 0) {
+    if (a > b) {
+      a %= b;
+    } else {
+      b %= a;
+    }
+  }
+
+  return cons(`${car(numbers)} ${cdr(numbers)}`, String(a + b));
+};
+
+const game = () => {
+  welcome(rule);
+  gameProcess(generateTask);
+};
+
+export default game;
